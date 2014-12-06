b9879ef4cb7c9b7a868ba8fb9cdbc5dac533f839
Kyle <kyle@tyrfingr.is>
2014-12-06 00:11:21 -0800

    Add blink example.

---
+++ 001-blink/blink.c
@@ -0,0 +1,54 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#define DELAY_S	300
+#define DELAY_O 600
+
+int	main(void) __attribute__((noreturn));
+
+
+static void
+s(void)
+{
+	unsigned char	i = 0;
+
+	for (i = 0; i < 3; i++) {
+		PORTB ^= _BV(PB0);
+		_delay_ms(DELAY_S);
+		PORTB ^= _BV(PB0);
+		_delay_ms(DELAY_S);
+	}
+	PORTB = ~_BV(PB0);
+	_delay_ms(500);
+}
+
+
+static void
+o(void)
+{
+	unsigned char	i = 0;
+
+	for (i = 0; i < 3; i++) {
+		PORTB ^= _BV(PB0);
+		_delay_ms(DELAY_O);
+		PORTB ^= _BV(PB0);
+		_delay_ms(DELAY_O);
+	}
+	PORTB = ~_BV(PB0);
+	_delay_ms(500);
+}
+
+
+int
+main(void)
+{
+	PORTB |= _BV(PB0);
+
+	while (1) {
+		s();
+		o();
+		s();
+		_delay_ms(1200);
+	}
+}
